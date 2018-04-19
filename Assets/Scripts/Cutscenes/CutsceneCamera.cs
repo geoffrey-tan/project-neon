94aafafda45015120bac9f6dd7956131651d33af
Geoffrey Tan <geoffreyt@live.nl>
2018-04-19 12:10:35 +0200

    Cutscene

---
+++ Assets/Scripts/Cutscenes/CutsceneCamera.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneCamera : MonoBehaviour
+{
+	// gameObjects
+	public GameObject playerCam;
+	public GameObject target;
+	private GameObject cutsceneCam;
+
+	// Variables
+	private Vector3 startPos, endPos;
+	private bool cutscene;
+
+	void Start()
+	{
+		cutsceneCam = transform.Find("Cutscene Camera").gameObject;
+
+		startPos = transform.Find("Camera Rail/StartPos").position;
+		endPos = transform.Find("Camera Rail/EndPos").position;
+
+		target.transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 2, target.transform.position.z);
+	}
+
+	void Update()
+	{
+		if (cutscene)
+		{
+			cutsceneCam.transform.position = Vector3.MoveTowards(cutsceneCam.transform.position, endPos, 1f * Time.deltaTime);
+			cutsceneCam.transform.LookAt(target.transform.position);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			Debug.Log("Hi");
+			playerCam.SetActive(false);
+			cutsceneCam.SetActive(true);
+
+			cutscene = true;
+		}
+	}
+
+	IEnumerator CutsceneDuration(float time)
+	{
+		yield return new WaitForSeconds(time);
+	}
+}
